4cd98224a6358e9401342776065391ebec5847b0
rosmeryqm2020-afk <rosmeryqm2020l@gmail.com>
2026-08-29 09:16:36 -0400

    Configurar base de datos sqlite y agregar columnas a productos

--- database/migrations/2026_08_29_130046_create_productos_table.php
+++ database/migrations/2026_08_29_130046_create_productos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+$table->string('nombre');
+$table->decimal('precio', 10, 2);
             $table->timestamps();
         });
     }
